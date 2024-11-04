2406a5321c516c53bcf48e1f47a26709f757a45f
Christine <guerrerochristinejoy15@gmail.com>
2024-11-04 21:54:04 +0800

    Initial commit

---
+++ classSubjects.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structures and Algorithms";
+let classSchedule = "9:30 AM - 10:30 AM, MF";
+let classroom = "Computer Lab A";
+let classInstructor = "Dr. Vitruvius John D. Barayuga";
+let studentName = "Christine Joy A. Guerrero";
+
+console.log(""+studentName+" is currently enrolled in "+subjectTitle+" with a class schedule of "+classSchedule+" at room "+classroom+". The instructor for the subject is "+classInstructor);
